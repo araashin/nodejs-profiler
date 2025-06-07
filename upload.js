a09a63e17b3393d50db5eb599f4bee3dee8ae42a
araashin <128715490+araashin@users.noreply.github.com>
2025-06-07 19:55:07 +0900

    Delete upload.js

--- upload.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-const parser = require('../utils/parser');
-
-module.exports = async (req, res) => {
-  const filePath = path.join(__dirname, '..', req.file.path);
-  const result = await parser(filePath);
-  res.render('result', { result });
-};
